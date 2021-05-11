8ee2527146241c6cc51b65dc31b54dd00fd35c0a
Stackbit Code Editor <projects@stackbit.com>
2021-05-11 19:45:35 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -13,7 +13,7 @@ export default class HeroSection extends React.Component {
                 {_.get(section, 'hero_items', null) && (
                     <div>
                      {_.map(_.get(section, 'hero_items', null), (item, item_idx) => (
-                        <div key={testimonial_idx} className="cell">
+                        <div key={item_idx} className="cell">
                         {_.get(item, 'image', null) && (
                         <img 
                             src={withPrefix(_.get(item, 'image', null))} 
