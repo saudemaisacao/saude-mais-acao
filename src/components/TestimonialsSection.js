097a8f378620eb5650f92f64447c0cdac1197cb1
Stackbit Code Editor <projects@stackbit.com>
2021-05-11 19:58:00 +0000

    TestimonialsSection.js: changed

--- src/components/TestimonialsSection.js
+++ src/components/TestimonialsSection.js
@@ -2,8 +2,6 @@ import React from 'react';
 import _ from 'lodash';
 
 import {withPrefix} from '../utils';
-import {markdownify} from '../utils';
-import CtaButtons from './CtaButtons';
 
 export default class TestimonialsSection extends React.Component {
     render() {
