d609b08a00fbe4d0b90a778f96295e4f6accfc2a
Stackbit Code Editor <projects@stackbit.com>
2021-05-11 17:48:51 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ import CtaButtons from './CtaButtons';
 export default class HeroSection extends React.Component {
     render() {
         let section = _.get(this.props, 'section', null);
-        let item = _.get(this.props, 'section', null);
+        let item = _.get(section, 'section', null);
         return (
             <section id={_.get(item, 'section_id', null)} className="hero">
                 <div id="carouselIndicators" className="carousel slide my-carousel my-carousel" data-ride="carousel">
