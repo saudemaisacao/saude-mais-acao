82b8e0a64bac1dfec61e9e274f77e8e969194723
Stackbit Code Editor <projects@stackbit.com>
2021-05-11 17:41:39 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -28,20 +28,6 @@ export default class HeroSection extends React.Component {
                         <span className="carousel-control-next-icon" aria-hidden="true"></span>
                     </a>
                 </div>
-
-                    {_.get(section, 'title', null) && (
-                    <h1 className="hero__title">{_.get(section, 'title', null)}</h1>
-                    )}
-                    {_.get(section, 'content', null) && (
-                    <div className="hero__body text-block">
-                    {markdownify(_.get(section, 'content', null))}
-                    </div>
-                    )}
-                    {_.get(section, 'actions', null) && (
-                    <div className="hero__actions button-group">
-                    <CtaButtons {...this.props} actions={_.get(section, 'actions', null)} />
-                    </div>
-                    )}
                 </div>
             </section>
         );
