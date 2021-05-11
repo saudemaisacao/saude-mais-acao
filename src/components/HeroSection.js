7898f4f3e00cc08b8e9e080e043ac999e57c82e7
Stackbit Code Editor <projects@stackbit.com>
2021-05-11 17:33:02 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -13,7 +13,7 @@ export default class HeroSection extends React.Component {
 
                 
                     <div id="carouselExampleIndicators" className="carousel slide my-carousel my-carousel" data-ride="carousel">
-                    <ol class="carousel-indicators">
+                    <ol className="carousel-indicators">
                         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
