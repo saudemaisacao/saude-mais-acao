c90a4036b1b349e3388307796e30a55772a11325
Stackbit Code Editor <projects@stackbit.com>
2021-05-11 18:09:34 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -29,7 +29,7 @@ export default class HeroSection extends React.Component {
                                         <CtaButtons {...this.props} actions={_.get(item, 'actions', null)} />
                                     )}
                             </div>
-                         );)
+                         ))
                     }   
                    </div>
                     <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
