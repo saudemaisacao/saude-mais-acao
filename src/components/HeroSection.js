3eb6520fa4ddc5278e1d69bc12feeb7cc8d373a5
Stackbit Code Editor <projects@stackbit.com>
2021-05-11 17:23:25 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -24,13 +24,13 @@ export default class HeroSection extends React.Component {
                 </div>
 
                 <div class="container">
-		<div class="carousel slide" id="main-carousel" data-ride="carousel">
-			<ol class="carousel-indicators">
-				<li data-target="#main-carousel" data-slide-to="0" class="active"></li>
-				<li data-target="#main-carousel" data-slide-to="1"></li>
-				<li data-target="#main-carousel" data-slide-to="2"></li>
-				<li data-target="#main-carousel" data-slide-to="3"></li>
-			</ol>
+                <div class="carousel slide" id="main-carousel" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#main-carousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#main-carousel" data-slide-to="1"></li>
+                        <li data-target="#main-carousel" data-slide-to="2"></li>
+                        <li data-target="#main-carousel" data-slide-to="3"></li>
+                    </ol>
 			
 			<div class="carousel-inner">
 				<div class="carousel-item active">
