85e51bb3c6e48d3a41274f575c9ad579a4a679d2
Stackbit Code Editor <projects@stackbit.com>
2021-05-11 17:23:33 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -32,46 +32,46 @@ export default class HeroSection extends React.Component {
                         <li data-target="#main-carousel" data-slide-to="3"></li>
                     </ol>
 			
-			<div class="carousel-inner">
-				<div class="carousel-item active">
-					<img class="d-block img-fluid" src="https://s19.postimg.cc/qzj5uncgj/slide1.jpg" alt="">
-					<div class="carousel-caption d-none d-md-block">
-						<h1>Mountain</h1>
-					</div>
-				</div>
-				<div class="carousel-item">
-					<img class="d-block img-fluid" src="https://s19.postimg.cc/lmubh3h0j/slide2.jpg" alt="">
-					<div class="carousel-caption d-none d-md-block">
-						<h3>Mountain</h3>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci esse vitae exercitationem fugit, numquam minus!</p>
-					</div>
-				</div>
-				<div class="carousel-item">
-					<img class="d-block img-fluid" src="https://s19.postimg.cc/99hh9lr5v/slide3.jpg" alt="">
-					<div class="carousel-caption d-none d-md-block">
-						<h3>Mountain</h3>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci esse vitae exercitationem fugit, numquam minus!</p>
-					</div>
-				</div>
-				<div class="carousel-item">
-					<img src="https://s19.postimg.cc/nenabzsnn/slide4.jpg" alt="" class="d-block img-fluid">
-					<div class="carousel-caption d-none d-md-block">
-						<h3>Mountain</h3>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci esse vitae exercitationem fugit, numquam minus!</p>
-					</div>
-				</div>
-			</div>
-			
-			<a href="#main-carousel" class="carousel-control-prev" data-slide="prev">
-				<span class="carousel-control-prev-icon"></span>
-				<span class="sr-only" aria-hidden="true">Prev</span>
-			</a>
-			<a href="#main-carousel" class="carousel-control-next" data-slide="next">
-				<span class="carousel-control-next-icon"></span>
-				<span class="sr-only" aria-hidden="true">Next</span>
-			</a>
-		</div>
-	</div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block img-fluid" src="https://s19.postimg.cc/qzj5uncgj/slide1.jpg" alt="">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h1>Mountain</h1>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block img-fluid" src="https://s19.postimg.cc/lmubh3h0j/slide2.jpg" alt="">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h3>Mountain</h3>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci esse vitae exercitationem fugit, numquam minus!</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block img-fluid" src="https://s19.postimg.cc/99hh9lr5v/slide3.jpg" alt="">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h3>Mountain</h3>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci esse vitae exercitationem fugit, numquam minus!</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://s19.postimg.cc/nenabzsnn/slide4.jpg" alt="" class="d-block img-fluid">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h3>Mountain</h3>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci esse vitae exercitationem fugit, numquam minus!</p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <a href="#main-carousel" class="carousel-control-prev" data-slide="prev">
+                            <span class="carousel-control-prev-icon"></span>
+                            <span class="sr-only" aria-hidden="true">Prev</span>
+                        </a>
+                        <a href="#main-carousel" class="carousel-control-next" data-slide="next">
+                            <span class="carousel-control-next-icon"></span>
+                            <span class="sr-only" aria-hidden="true">Next</span>
+                        </a>
+                    </div>
+                </div>
                 )}
               </div>
             </section>
