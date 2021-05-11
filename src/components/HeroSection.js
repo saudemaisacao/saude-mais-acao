0f2c539b6d93ea1f3de1ca25fda0023a507294a1
Stackbit Code Editor <projects@stackbit.com>
2021-05-11 19:50:10 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -19,7 +19,8 @@ export default class HeroSection extends React.Component {
                                 alt={_.get(item, 'image_alt', null)}
                             />
                         )}
-                         <div className="container container--lg"><div>
+                         <div className="container container--lg">
+                         <div>
                         </div>
                      ))}
                     </div>
