3d50c1856322cb6399d71b4ce539e61a16dfeaa1
Stackbit Code Editor <projects@stackbit.com>
2021-05-11 19:48:00 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
 
-import {markdownify} from '../utils';
 import CtaButtons from './CtaButtons';
 
 export default class HeroSection extends React.Component {
