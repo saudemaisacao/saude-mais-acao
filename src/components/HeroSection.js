c6f36932c9d43b30513914f2b85faa098f3f6c04
Stackbit Code Editor <projects@stackbit.com>
2021-05-12 02:29:09 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -42,6 +42,7 @@ export default class HeroSection extends React.Component {
                     </div>
                 )}
             </div>
+             {_.map(_.get(section, 'hero_items', null), (item, item_idx) => (
             <input type="radio" class="cs_anchor radio" name="slider" id="slide1"/>
             </section>
         );
